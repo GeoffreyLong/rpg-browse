0a3b93d8e1151b8b1913e1e0373557df6949f2b5
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-11-12 23:56:12 -0500

    ADD initial data storage

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,17 @@
 var app = angular.module("appGui", []);
 app.controller("myCtrl", function($scope) {
-  $scope.temp = "hello world";
+  $scope.items = [];
+  getItems();
+  
+  // Should probably encapsulate this in a service?
+  function getItems() {
+    chrome.storage.sync.get("packStorage", function(obj) {
+      $scope.items = $scope.items.concat(obj["packStorage"]);
+      $scope.$digest();
+    });
+    chrome.storage.sync.get("homeStorage", function(obj) {
+      $scope.items = $scope.items.concat(obj["homeStorage"]);
+      $scope.$digest();
+    });
+  }
 });
