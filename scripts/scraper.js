ad448bcb40086cea7867f49f0dd0e78617606012
Huang Ying-Kai <pidog1993@gmail.com>
2016-11-13 07:46:14 -0500

    changes keywords to buttons and returns them in an array

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -5,95 +5,32 @@
 //      don't want to run before full load
 //      Might also be able to do this via the chrome API 
 console.log("Scraper Running");
+var keywords = ["sword", "gold", "yellow", "blue", "green", "china", "civil", "state"];
+var itemStorage = [ ];
 
-// This will hold all of the scraping logic
+// This will match the keywords with the page textx
 // Will also create the necessary buttons
 (function() {
   function runScraper() {
     console.log($('body'));
-
-    // Scrape the crap out of the page here
-    // Inject buttons when necessary
-    //    Make calls to createResourceButton or createActionButton
-    // Be disruptive
-    // Make money
-    // Change the world
-
-    var wordArray = [" "];
-    var paragraphText = $('body').find(":not(script)");
-    var tags = ["2"];
-
-    /*
-    for(var i =0; i < paragraphText.length; i++)
-    {
-      tags.push(findDeepestChild(paragraphText[i]));
-      wordArray.push(paragraphText[i].innerText.split("/\W/"));
-      if (paragraphText[i].innerText.length > 7 )
-      {
-          var inHtml = $(paragraphText[i]).html();
-          if (inHtml) {
-
-            /*
-        console.log(inHtml);
-          var button = "</p>" + "<button> civil </button>" + "<p>";
-          
-          $(paragraphText[i]).html(inHtml.replace(/civil/gi, button));
-          
-
-
-          }
-      }
-    }
-    */
-    
-    for(var i =0; i < paragraphText.length; i++)
-    {
-      tags.push(findDeepestChild(paragraphText[i]));
-    }
-
-    for(var i = 0; i < tags.length; i++)
-    {
-
-      wordArray.push(tags[i].element);
-    }
-
-   
-
-
-
-    console.log(tags);  
-    console.log(paragraphText);
-    console.log(wordArray);
+     for(var i = 0; i < keywords.length; i++){
+     $("body:not([href]):not(:image)").html($("body:not([href]):not(:image)").html().replace(new RegExp(keywords[i], "ig"),"<button onclick = "buttonInfo()"> " + keywords[i] + " </button>"));
+     console.log("Ran it " + i);
+  }
 
   }
 
-  function createResourceButton() {
-    // Programatically create a button here
 
-    // Really want to return the button
-    return null;
+  function getButtonInfo(){
+    itemStorage.push($('button.text').text());
+    
+
+
   }
   function createActionButton() {
   }
 
-  function findDeepestChild(parent) {
-
-    var result = {depth: 0, element: parent};
-
-    $(parent) .children().each(
-        function(idx) {
-            var child = $(this);
-            var childResult = findDeepestChild(child);
-            if (childResult.depth + 1 > result.depth) {
-                result = {
-                    depth: 1 + childResult.depth, 
-                    element: childResult.element};
-            }
-        }
-    );
-
-    return result;
-}
+  
 
 
 
